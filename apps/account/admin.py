57c1a35fc3da208ee98867075dbb30716fe2b6ae
asmaur <maur.a@outlook.com>
2019-07-11 07:46:58 -0300

    api v1

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Agente
+from .models import *
 from django.contrib.auth.models import User
 # Register your models here.
 
 admin.site.register(Agente)
 
+admin.site.register(UserBalance)
