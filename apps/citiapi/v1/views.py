5a0c0043a0948f6f2a32a54a9d902453a59db893
A. Maurice <maur.a@outlook.com>
2019-07-26 02:02:49 -0300

    api v1

--- apps/citiapi/v1/views.py
+++ apps/citiapi/v1/views.py
@@ -32,7 +32,7 @@ class Subscriptions(viewsets.ViewSet):
     def general(self, request):
         """"Conjunto de 12 modelo na pagina principal global excluindo as subs basicas"""
 
-        queryset = Subscription.subs_active.filter(perfil__category="mulheres").filter(perfil__is_working=True).filter(perfil__suspended=False).order_by("types")
+        queryset = Subscription.subs_active.filter(perfil__category="mulheres").filter(perfil__is_working=True).filter(perfil__suspended=False).order_by("types")[:12]
         serializer = SubscriptionSerializer(queryset, many=True, )
         #print(queryset)
         return Response(serializer.data)
